cb694c8de5cfc91de0845d956713bc42d80abe39
Nicolás Suárez <nicolas.suarez.vera@gmail.com>
2025-11-30 07:21:50 -0500

    feat: Create public incident page to display limited details and offer Android app download

--- src/app/incidents/[id]/page.tsx
+++ src/app/incidents/[id]/page.tsx
@@ -52,7 +52,6 @@ export default function IncidentPublicPage() {
   }, [params.id]);
 
   const handleDownload = () => {
-    console.log(APK_DOWNLOAD_URL)
     if (isAndroid) {
       const link = document.createElement('a');
       link.href = APK_DOWNLOAD_URL;
